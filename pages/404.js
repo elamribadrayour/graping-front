406f0f63c4d8080f68944e5e3e7f47da1f4d32c8
amya timidi <amya.timidi@protonmail.com>
2022-08-24 15:00:24 +0200

    fix className

--- pages/404.js
+++ pages/404.js
@@ -5,19 +5,19 @@ export default function not_found() {
     return (
         <div>
             <Header />
-            <div class={Background() + " text-white"}>
-                <div class="flex min-h-screen text-center 
+            <div className={Background() + " text-white"}>
+                <div className="flex min-h-screen text-center 
                             flex-col space-y-10
                             items-center justify-center
                             md:space-x-10
                             md:flex-row md:space-y-0">
                     <a href="/404"
-                        class="hover:text-black hover:bg-white border-whit
+                        className="hover:text-black hover:bg-white border-whit
                                     rounded-xl py-2 px-4 border 
                                     duration-1000">
                         Retry</a>
                     <a href="/"
-                        class="hover:text-black hover:bg-white border-white 
+                        className="hover:text-black hover:bg-white border-white 
                                     rounded-xl py-2 px-4 border 
                                     duration-1000">
                         Home</a>
